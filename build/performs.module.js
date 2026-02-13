d30faed4a3785ce36f88a6f266294e20d1367ffc
Carolina del Corral <carolina.cdc@outlook.com>
2026-02-13 14:28:20 +0100

    fix multicamera recorder

--- build/performs.module.js
+++ build/performs.module.js
@@ -2542,14 +2542,15 @@ class AnimationRecorder {
     constructor(numCameras, app) {
         this.isRecording = false;
         this.timeLimit = null;
+        this.clock = new THREE.Clock();
+        // for each camera:
         this.mediaRecorders = [];
         this.recordedChunks = [];
         this.renderers = [];
-        this.clock = new THREE.Clock();
+        
         this.handleDataAvailable = this.handleDataAvailable.bind(this);
         this.handleStop = this.handleStop.bind(this);
-        this.animationsCount = 0;
-        this.enabledCameras = 0;
+        
         this.exportZip = true;
 
         for (let i = 0; i < numCameras; i++) {
@@ -2567,17 +2568,24 @@ class AnimationRecorder {
             const mediaRecorder = new MediaRecorder(stream, options);
             mediaRecorder.ondataavailable = (event) => this.handleDataAvailable(event, i);
             mediaRecorder.onstop = () => this.handleStop(i);
-            mediaRecorder.onstart = () => this.handleStart(i);
 
             this.mediaRecorders.push( mediaRecorder );
             this.recordedChunks.push([]);
         }        this.app = app;
+        window.addEventListener('resize', this._onWindowResize.bind(this));
+    }
+
+    _onWindowResize() {
+        for (let i = 0; i < this.renderers.length; i++) {
+            this.renderers[i].setSize(window.innerWidth, window.innerHeight);
+        }
     }
 
     async manageMultipleCapture (keyframeApp) {
         this.keyframeApp = keyframeApp;
         let animations = [];
         
+        // fill with animations that need to be recorded
         for (let animationName in keyframeApp.loadedAnimations) {
             let animation = keyframeApp.loadedAnimations[animationName];
             if (!animation.record) {
@@ -2585,11 +2593,13 @@ class AnimationRecorder {
             }
             animations.push(animationName);
         }
-        this.animationsCount = animations.length;
+        this.remainingAnimations = animations.length;
 
+        // record each animation sequentially
         for (let i = 0; i < animations.length; i++) {
             const animationName = animations[i];
             let animation = keyframeApp.loadedAnimations[animationName];
+            // gui update with current animation name
             if(this.onStartCapture) {
                 this.onStartCapture('(' + (i+1) + '/' + animations.length+ ') ' + animationName);
             }
@@ -2599,15 +2609,11 @@ class AnimationRecorder {
 
     manageCapture (animationName, timeLimit = null) {
         if (this.app.mode == PERFORMS.Modes.SCRIPT){
-            this.animationsCount = 1;
             if(this.onStartCapture) {
                 this.onStartCapture('');
             }
             if (this.isRecording) { 
-                this.stopCapture(); 
-                // if(this.onStopCapture) {
-                //     this.onStopCapture();
-                // }
+                this.stopCapture();
             }
             else { this.startCapture("BML"); }
         }
@@ -2630,23 +2636,48 @@ class AnimationRecorder {
 
     startCapture (animationName) {
         this.isRecording = true;
-        this.enabledCameras = 0;
+        this.currentAnimationName = animationName; // Store the animation name
+
+        this.enabledRecorders = [];
+
         for( let i = 0; i < this.app.cameras.length; i++) {
-            if(!this.app.cameras[i].record) {
-                continue;
-            }
-            this.enabledCameras += 1;
+            if(!this.app.cameras[i].record) continue;
+
             this.recordedChunks[i] = [];
+            this.enabledRecorders.push(i);
             this.mediaRecorders[i].start();
         }
-        // this.recordedChunks.forEach((chunk, i, arr) => arr[i] = []); // reset chuncks
-        // this.mediaRecorders.forEach(recorder => { recorder.start() });
-        this.currentAnimationName = animationName; // Store the animation name
+        
+        // start animation
+        if (this.app.mode == PERFORMS.Modes.SCRIPT){
+            this.app.scriptApp.replay();
+        }
+        else if (this.app.mode == PERFORMS.Modes.KEYFRAME) {
+            this.app.keyframeApp.changePlayState(true); // start animation                
+        }
+    
+        this.clock.start();
+
+        // expected files to be downloaded - for current animation
+        this.pendingFiles = this.enabledRecorders.length;
     }
         
     stopCapture () {
         this.isRecording = false;
-        this.mediaRecorders.forEach(recorder => recorder.stop());   
+
+        this.enabledRecorders.forEach(idx => {
+            if (this.mediaRecorders[idx].state === 'recording') {
+                this.mediaRecorders[idx].stop();
+            }
+        });
+
+        if (this.onStopCapture){
+            this.onStopCapture();
+            // reset pose on final recording stop
+            if (this.app.mode == PERFORMS.Modes.SCRIPT){
+                this.app.scriptApp.ECAcontroller.reset(true);
+            }
+        }
     }
 
     handleDataAvailable (event, idx) {
@@ -2655,84 +2686,69 @@ class AnimationRecorder {
         }
     }
 
-    handleStart (idx) {
-        if (idx === 0) {
-            if (this.app.mode == PERFORMS.Modes.SCRIPT){
-                this.app.scriptApp.replay();
-            }
-            else if (this.app.mode == PERFORMS.Modes.KEYFRAME) {
-                this.app.keyframeApp.changePlayState(true); // start animation                
-            }
-        }
-        this.clock.start();
-    }
-
     handleStop (idx) {
+        if (!this.enabledRecorders.includes(idx)) {
+            return; // Skip if this recorder was not enabled for recording
+        }
+
         const animationName = this.currentAnimationName;
         const blob = new Blob(this.recordedChunks[idx], {type: 'video/webm'});
-        const name =  `${animationName} ${idx + 1}.webm`;
+        const camNumber = idx + 1;
+        const name =  `${animationName} ${camNumber}.webm`;
 
-        blobToBase64(blob, (binaryData) => {
-            if(!zip && this.exportZip) {
-                console.error("JSZip not imported. The recordings can't be downloaded.");
-            }
+        if(!zip && this.exportZip) {
+            console.error("JSZip not imported. The recordings can't be downloaded.");
+        }
 
-            if(zip && this.exportZip) {
-                // Add downloaded file video to zip in the specified folder:
-                zip.folder(animationName).file(name, binaryData, {base64: true});
-                let files = Object.keys(zip.files);
-    
-                if((files.length - this.animationsCount) == this.animationsCount * this.enabledCameras) {
-                    if(this.onStopCapture) {
-                        this.onStopCapture();
-                    }
-                    // All files have been downloaded, create the zip and download it
-                    zip.generateAsync({type:"base64"}).then(function (base64) {
-                        let zipName = 'performs-recordings.zip';
-                        let a = document.createElement('a'); 
-                        // Then trigger the download link
-                        a.href = "data:application/zip;base64," + base64;
-                        a.download = zipName;
-                        a.click();
-                        zip.files = {};
-                    });
+        if(zip && this.exportZip) {
+            // Add downloaded file video to zip in the specified folder:
+            zip.folder(animationName).file(name, blob);
+        }
+        else {
+            let a = document.createElement('a'); 
+            // Then trigger the download link
+            a.href = URL.createObjectURL(blob);
+            a.download = name;
+            a.click();
+        }
+
+        this.pendingFiles--;
+
+        // if all files for current animation are ready, resolve promise to start next animation
+        if (this.pendingFiles === 0) {
+            
+            if (this.onCaptureComplete) {
+                this.onCaptureComplete();
+                this.onCaptureComplete = null; // reset callback for next animation
+
+                // refresh gui
+                if (this.app.mode == PERFORMS.Modes.SCRIPT) {
+                    // reset avatar pose / stop animation
+                    this.app.scriptApp.ECAcontroller.reset(true);
                 }
             }
-            else {
-                let a = document.createElement('a'); 
-                // Then trigger the download link
-                a.href = "data:application/webm;base64," + binaryData;
-                a.download = name;
-                a.click();
-
-                if(this.isRecording == false) {
-                    if(this.onStopCapture) {
-                        this.onStopCapture();
-                    }
-                }
-            }
-            //this.recordedChunks[idx] = [];
-        });
-
-        // refresh gui
-        if (idx === 0) {
-            if (this.app.mode == PERFORMS.Modes.SCRIPT) {
-                // reset avatar pose / stop animation
-                this.app.scriptApp.ECAcontroller.reset(true);
+            
+            // zip after ALL animations are recorded
+            this.remainingAnimations--;
+            if (this.remainingAnimations === 0 && zip && this.exportZip) {
+                this.downloadZip();
             }
         }
 
         // reset clock to 0
-        this.clock.elapsedTime = 0;
         this.clock.stop();
+        this.clock.elapsedTime = 0;
+    }
 
-        // Check if all recorders have stopped
-        if (this.mediaRecorders.every(recorder => recorder.state === 'inactive')) {
-            if (this.onCaptureComplete) {
-                this.onCaptureComplete(); // Resolve the promise to indicate that capture is complete
-                this.onCaptureComplete = null; // Clear the reference
-            }
-        }
+    async downloadZip() {
+        const blob = await zip.generateAsync({type: 'blob'});
+
+        const a = document.createElement('a');
+        a.href = URL.createObjectURL(blob);
+        a.download = 'recordings.zip';
+        a.click();
+
+        zip = new JSZip(); // reset zip for next recordings
     }
 
     update (scene, cameras) {
@@ -2742,20 +2758,12 @@ class AnimationRecorder {
         }
 
         if (this.timeLimit && this.clock.getElapsedTime() > this.timeLimit ) {
-            this.app.keyframeApp.changePlayState(false);  // stop animation
+            if(this.app.mode == PERFORMS.Modes.KEYFRAME) {
+                this.app.keyframeApp.changePlayState(false);  // stop animation
+            }
             this.stopCapture();
         }
     }
-}
-
-function blobToBase64(blob, callback) {
-    var reader = new FileReader();
-    reader.onload = function() {
-        var dataUrl = reader.result;
-        var base64 = dataUrl.split(',')[1];
-        callback(base64);
-    };
-    reader.readAsDataURL(blob);
 }
 
 // This is a generated file. Do not edit. 
